940643319bf1e9262634253c4d77653a8c51d3e5
ashraffares <ashraffares090@gmail.com>
2021-04-10 17:39:12 +0200

    Change root to be friends/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :friends
   devise_for :users
-  root 'home#index'
+  root 'friends#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
