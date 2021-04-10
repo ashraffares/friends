c4b1117536d127edbb313bc31301b61300c289d5
ashraffares <ashraffares090@gmail.com>
2021-04-10 18:53:25 +0200

    Add more validation to friends

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -3,6 +3,6 @@ class Friend < ApplicationRecord
   validates :first_name, presence: true, length: { minimum: 2 }
   validates :last_name, presence: true, length: { minimum: 2 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :phone, presence: true, uniqueness: true
+  validates :phone, presence: true, uniqueness: true, :numericality => true, :length => { :minimum => 11, :maximum => 15 }
   validates :twitter, presence: true, uniqueness: true
 end
