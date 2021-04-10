477695acdb9a00e13804a5d8a0e466dd2e2d3ab7
ashraffares <ashraffares090@gmail.com>
2021-04-10 17:49:08 +0200

    don't allow user to do anything unless authenticated

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,6 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /friends or /friends.json
   def index
