06eb0d157a3f822718526942266ec05660149f33
ashraffares <ashraffares090@gmail.com>
2021-04-10 15:10:22 +0200

    Add friends validation

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,8 @@
 class Friend < ApplicationRecord
   belongs_to :user
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone, presence: true, uniqueness: true
+  validates :twitter, presence: true, uniqueness: true
 end
