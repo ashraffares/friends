66edb7f888fc9a804f1ba04358c1b52870c0953d
ashraffares <ashraffares090@gmail.com>
2021-04-11 13:13:46 +0200

    Show welcome screen for not loged in users

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,6 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[show edit update destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[show index]
   before_action :correct_user, only: %i[edit update destroy]
 
   # GET /friends or /friends.json
